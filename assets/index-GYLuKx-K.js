a680801f9a467f467addb7911f9d0d4ea4dddaef
OPBS Liyana <liyana.perso@gmail.com>
2026-03-13 15:22:17 -0400

    désactiver préchargement

---
+++ assets/index-GYLuKx-K.js
@@ -0,0 +1,6 @@
+import { importShared } from './__federation_fn_import-dmqIiFee.js';
+import App from './__federation_expose_ListeView-umwxsYqN.js';
+
+const {createApp} = await importShared('vue');
+
+createApp(App).mount('#app');
