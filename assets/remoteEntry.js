9ff83bce5cf59a4c0629018a98deb79f572e63a2
OPBS Liyana <liyana.perso@gmail.com>
2026-03-30 15:11:27 -0400

    enregistrement des nom de liste

--- assets/remoteEntry.js
+++ assets/remoteEntry.js
@@ -2,8 +2,8 @@ const currentImports = {};
       const exportSet = new Set(['Module', '__esModule', 'default', '_export_sfc']);
       let moduleMap = {
 "./ListeView":()=>{
-      dynamicLoadingCss(["style-v3yWr5Zt.css"], false, './ListeView');
-      return __federation_import('/applist-dist/assets/__federation_expose_ListeView-BH0AwNdj.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},};
+      dynamicLoadingCss(["style-DdF9abIz.css"], false, './ListeView');
+      return __federation_import('/applist-dist/assets/__federation_expose_ListeView-UjpagQND.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},};
       const seen = {};
       const dynamicLoadingCss = (cssFilePaths, dontAppendStylesToHead, exposeItemName) => {
         const metaUrl = import.meta.url;
