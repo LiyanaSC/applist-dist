909c03ab74fc312a4eddb590f19f0acbbc3e3b16
OPBS Liyana <liyana.perso@gmail.com>
2026-03-31 13:11:11 -0400

    retrait img non uploadé

---
+++ assets/index-CfKrNRhP.js
@@ -0,0 +1,47 @@
+import { importShared } from './__federation_fn_import-dmqIiFee.js';
+import ListeView, { _ as _export_sfc, c as createPinia } from './__federation_expose_ListeView-HVeKA6_M.js';
+
+const {createElementVNode:_createElementVNode,createVNode:_createVNode,openBlock:_openBlock,createElementBlock:_createElementBlock} = await importShared('vue');
+
+
+const _hoisted_1 = { class: "app-liste" };
+
+
+const _sfc_main = {
+  __name: 'App',
+  setup(__props) {
+
+//import Sidebar from './Layout/Sidebar.vue';
+//import ListResults from './components/ListResults.vue';
+const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches; // Vérifie si l'utilisateur préfère le mode sombre
+// Si l'utilisateur préfère le mode sombre, on applique le thème sombre, sinon on applique le thème clair
+document.documentElement.setAttribute('data-theme', prefersDark ? 'dark' : 'light');
+
+return (_ctx, _cache) => {
+  return (_openBlock(), _createElementBlock("div", _hoisted_1, [
+    _cache[0] || (_cache[0] = _createElementVNode("h1", null, "Applications distante:", -1)),
+    _createVNode(ListeView)
+  ]))
+}
+}
+
+};
+const App = /*#__PURE__*/_export_sfc(_sfc_main, [['__scopeId',"data-v-c398b66c"]]);
+
+const {createApp} = await importShared('vue');
+
+const app = createApp(App);
+
+const pinia = createPinia();
+
+app.use(pinia); // 🔥 OBLIGATOIRE
+
+app.mount('#app');
+
+window.addEventListener('error', function (event) {
+  console.error("Erreur JavaScript capturée :", event.message);
+  console.log("Fichier :", event.filename);
+  console.log("Ligne :", event.lineno);
+  console.log("Colonne :", event.colno);
+  console.log("Objet erreur complet :", event.error);
+});
