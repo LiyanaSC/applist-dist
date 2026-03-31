8171ea8c2cbdaa1e1e1af11789d8fb68f460d32f
OPBS Liyana <liyana.perso@gmail.com>
2026-03-31 12:58:11 -0400

    bug visuel federation

--- assets/remoteEntry.js
+++ assets/remoteEntry.js
@@ -2,8 +2,8 @@ const currentImports = {};
       const exportSet = new Set(['Module', '__esModule', 'default', '_export_sfc']);
       let moduleMap = {
 "./ListeView":()=>{
-      dynamicLoadingCss(["style-D8u7VBcW.css"], false, './ListeView');
-      return __federation_import('/applist-dist/assets/__federation_expose_ListeView-C2B71Ph6.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},};
+      dynamicLoadingCss(["style-CS-UmPWe.css"], false, './ListeView');
+      return __federation_import('/applist-dist/assets/__federation_expose_ListeView-BxnxsiWs.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},};
       const seen = {};
       const dynamicLoadingCss = (cssFilePaths, dontAppendStylesToHead, exposeItemName) => {
         const metaUrl = import.meta.url;
