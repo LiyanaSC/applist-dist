1837845748eb412d32b60b4de5bc2a35199467d1
OPBS Liyana <liyana.perso@gmail.com>
2026-04-20 11:00:54 -0400

    correction visuel

--- assets/remoteEntry.js
+++ assets/remoteEntry.js
@@ -3,7 +3,7 @@ const currentImports = {};
       let moduleMap = {
 "./mount":()=>{
       dynamicLoadingCss(["style-YJhN9lpL.css"], false, './mount');
-      return __federation_import('/applist-dist/assets/__federation_expose_Mount-CSIenmHG.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},};
+      return __federation_import('/applist-dist/assets/__federation_expose_Mount-Cjv962Rt.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},};
       const seen = {};
       const dynamicLoadingCss = (cssFilePaths, dontAppendStylesToHead, exposeItemName) => {
         const metaUrl = import.meta.url;
