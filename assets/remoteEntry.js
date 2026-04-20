97f01de220ed0e0138e208916d0bca9f2e9ac63d
OPBS Liyana <liyana.perso@gmail.com>
2026-04-20 14:48:13 -0400

    try applist

--- assets/remoteEntry.js
+++ assets/remoteEntry.js
@@ -2,8 +2,8 @@ const currentImports = {};
       const exportSet = new Set(['Module', '__esModule', 'default', '_export_sfc']);
       let moduleMap = {
 "./mount":()=>{
-      dynamicLoadingCss(["style-BPh_D8ya.css"], false, './mount');
-      return __federation_import('/applist-dist/assets/__federation_expose_Mount-C_SIub-9.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},};
+      dynamicLoadingCss(["style-PcGuQJx0.css"], false, './mount');
+      return __federation_import('/applist-dist/assets/__federation_expose_Mount-Bpcv9NGS.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},};
       const seen = {};
       const dynamicLoadingCss = (cssFilePaths, dontAppendStylesToHead, exposeItemName) => {
         const metaUrl = import.meta.url;
