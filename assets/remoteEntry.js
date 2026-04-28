298d8081b82872266f04dcbe0266cffec8ce20ff
OPBS Liyana <liyana.perso@gmail.com>
2026-04-28 15:52:33 -0400

    maj : delete other types keep classic

--- assets/remoteEntry.js
+++ assets/remoteEntry.js
@@ -2,8 +2,8 @@ const currentImports = {};
       const exportSet = new Set(['Module', '__esModule', 'default', '_export_sfc']);
       let moduleMap = {
 "./mount":()=>{
-      dynamicLoadingCss(["style-Cj_kapc2.css"], false, './mount');
-      return __federation_import('/applist-dist/assets/__federation_expose_Mount-BGzjR4SL.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},};
+      dynamicLoadingCss(["style-CDls0Aim.css"], false, './mount');
+      return __federation_import('/applist-dist/assets/__federation_expose_Mount-BO6W1txZ.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},};
       const seen = {};
       const dynamicLoadingCss = (cssFilePaths, dontAppendStylesToHead, exposeItemName) => {
         const metaUrl = import.meta.url;
