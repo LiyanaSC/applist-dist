c2894970847bbc1e71ddb71c0c9b9591fd175790
OPBS Liyana <liyana.perso@gmail.com>
2026-05-11 13:33:09 -0400

    fix: correction keyup mobile

--- assets/remoteEntry.js
+++ assets/remoteEntry.js
@@ -2,8 +2,8 @@ const currentImports = {};
       const exportSet = new Set(['Module', '__esModule', 'default', '_export_sfc']);
       let moduleMap = {
 "./mount":()=>{
-      dynamicLoadingCss(["style-SnsuSQYK.css"], false, './mount');
-      return __federation_import('/applist-dist/assets/__federation_expose_Mount-BxU_O2N_.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},};
+      dynamicLoadingCss(["style-DLj8R587.css"], false, './mount');
+      return __federation_import('/applist-dist/assets/__federation_expose_Mount-Bgg-8YYb.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},};
       const seen = {};
       const dynamicLoadingCss = (cssFilePaths, dontAppendStylesToHead, exposeItemName) => {
         const metaUrl = import.meta.url;
