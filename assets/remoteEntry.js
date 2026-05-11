4fcf23a45b2aad4ba75994d73dbd412d8951b3d0
OPBS Liyana <liyana.perso@gmail.com>
2026-05-11 12:49:20 -0400

    updating des résultat de la liste

--- assets/remoteEntry.js
+++ assets/remoteEntry.js
@@ -2,8 +2,8 @@ const currentImports = {};
       const exportSet = new Set(['Module', '__esModule', 'default', '_export_sfc']);
       let moduleMap = {
 "./mount":()=>{
-      dynamicLoadingCss(["style-CfQ6qlpL.css"], false, './mount');
-      return __federation_import('/applist-dist/assets/__federation_expose_Mount-CLNZYHtJ.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},};
+      dynamicLoadingCss(["style-d7SWElmH.css"], false, './mount');
+      return __federation_import('/applist-dist/assets/__federation_expose_Mount-TwwdOEHV.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},};
       const seen = {};
       const dynamicLoadingCss = (cssFilePaths, dontAppendStylesToHead, exposeItemName) => {
         const metaUrl = import.meta.url;
