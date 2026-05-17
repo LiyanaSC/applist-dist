f8e078403a83f9375041ff0b8a4571f9a91954d0
OPBS Liyana <liyana.perso@gmail.com>
2026-05-17 11:06:13 -0400

    test width 100 and bk transparent list sidebar

--- assets/remoteEntry.js
+++ assets/remoteEntry.js
@@ -2,8 +2,8 @@ const currentImports = {};
       const exportSet = new Set(['Module', '__esModule', 'default', '_export_sfc']);
       let moduleMap = {
 "./mount":()=>{
-      dynamicLoadingCss(["style-BZTkqEyv.css"], false, './mount');
-      return __federation_import('/applist-dist/assets/__federation_expose_Mount-DoOkqFGa.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},};
+      dynamicLoadingCss(["style-ygxH9PP6.css"], false, './mount');
+      return __federation_import('/applist-dist/assets/__federation_expose_Mount-CfZOCF_S.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},};
       const seen = {};
       const dynamicLoadingCss = (cssFilePaths, dontAppendStylesToHead, exposeItemName) => {
         const metaUrl = import.meta.url;
