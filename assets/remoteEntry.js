84a08542a0b4f45ebbb3bff309367ab73792fd97
OPBS Liyana <liyana.perso@gmail.com>
2026-05-17 11:14:03 -0400

    style : fix bug btn

--- assets/remoteEntry.js
+++ assets/remoteEntry.js
@@ -2,8 +2,8 @@ const currentImports = {};
       const exportSet = new Set(['Module', '__esModule', 'default', '_export_sfc']);
       let moduleMap = {
 "./mount":()=>{
-      dynamicLoadingCss(["style-ygxH9PP6.css"], false, './mount');
-      return __federation_import('/applist-dist/assets/__federation_expose_Mount-CfZOCF_S.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},};
+      dynamicLoadingCss(["style-CZjIYN4X.css"], false, './mount');
+      return __federation_import('/applist-dist/assets/__federation_expose_Mount-B5b4O_rc.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},};
       const seen = {};
       const dynamicLoadingCss = (cssFilePaths, dontAppendStylesToHead, exposeItemName) => {
         const metaUrl = import.meta.url;
