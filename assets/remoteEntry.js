d9669f9aa92f07c4031a2abf4ba0bf1cab6e5e84
OPBS Liyana <liyana.perso@gmail.com>
2026-05-26 12:08:29 -0400

    update create subtitle blod

--- assets/remoteEntry.js
+++ assets/remoteEntry.js
@@ -2,8 +2,8 @@ const currentImports = {};
       const exportSet = new Set(['Module', '__esModule', 'default', '_export_sfc']);
       let moduleMap = {
 "./mount":()=>{
-      dynamicLoadingCss(["style-Da3FVPTf.css"], false, './mount');
-      return __federation_import('/applist-dist/assets/__federation_expose_Mount-DCljqWUa.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},};
+      dynamicLoadingCss(["style-CAcGYCQx.css"], false, './mount');
+      return __federation_import('/applist-dist/assets/__federation_expose_Mount-Co7QaDm4.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},};
       const seen = {};
       const dynamicLoadingCss = (cssFilePaths, dontAppendStylesToHead, exposeItemName) => {
         const metaUrl = import.meta.url;
