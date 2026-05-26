c79a3009c11dfe82cf59104ddb559a4b18903f44
OPBS Liyana <liyana.perso@gmail.com>
2026-05-26 11:29:41 -0400

    ux: cohérence style btn nouveau

--- assets/remoteEntry.js
+++ assets/remoteEntry.js
@@ -2,8 +2,8 @@ const currentImports = {};
       const exportSet = new Set(['Module', '__esModule', 'default', '_export_sfc']);
       let moduleMap = {
 "./mount":()=>{
-      dynamicLoadingCss(["style-718pXzRn.css"], false, './mount');
-      return __federation_import('/applist-dist/assets/__federation_expose_Mount-DQCU5sKi.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},};
+      dynamicLoadingCss(["style-Da3FVPTf.css"], false, './mount');
+      return __federation_import('/applist-dist/assets/__federation_expose_Mount-DCljqWUa.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},};
       const seen = {};
       const dynamicLoadingCss = (cssFilePaths, dontAppendStylesToHead, exposeItemName) => {
         const metaUrl = import.meta.url;
