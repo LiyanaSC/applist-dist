cee4d9887b3d42760f5405b855879e6320f1b7d8
OPBS Liyana <liyana.perso@gmail.com>
2026-05-27 19:00:29 -0400

    update styme fullscreen

--- assets/remoteEntry.js
+++ assets/remoteEntry.js
@@ -2,8 +2,8 @@ const currentImports = {};
       const exportSet = new Set(['Module', '__esModule', 'default', '_export_sfc']);
       let moduleMap = {
 "./mount":()=>{
-      dynamicLoadingCss(["style-CAcGYCQx.css"], false, './mount');
-      return __federation_import('/applist-dist/assets/__federation_expose_Mount-Co7QaDm4.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},};
+      dynamicLoadingCss(["style-DEP6Zf4M.css"], false, './mount');
+      return __federation_import('/applist-dist/assets/__federation_expose_Mount-CzzBnaLN.js').then(module =>Object.keys(module).every(item => exportSet.has(item)) ? () => module.default : () => module)},};
       const seen = {};
       const dynamicLoadingCss = (cssFilePaths, dontAppendStylesToHead, exposeItemName) => {
         const metaUrl = import.meta.url;
